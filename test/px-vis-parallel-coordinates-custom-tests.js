b54ebde01e271d3ac8a5789a48d979b44766fc4b
Benoit Chevalier <benoit.chevalier@ge.com>
2016-08-04 13:17:51 -0700

    Added travis config

--- test/px-vis-parallel-coordinates-custom-tests.js
+++ test/px-vis-parallel-coordinates-custom-tests.js
@@ -5,23 +5,6 @@ function runCustomTests() {
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
   suite('Custom Automation Tests for px-vis-parallel-coordinates', function() {
-    test('Check initial value of counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-parallel-coordinates'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-      done();
-    });
-
-    test('Clicking px-vis-parallel-coordinates increments the counter', function(done){
-      var counterEl = Polymer.dom(document).querySelector('px-vis-parallel-coordinates'),
-          counterValueEl = Polymer.dom(counterEl.root).querySelector('span');
-      assert.equal(counterValueEl.textContent, '0');
-
-      counterEl.click();
-      flush(function(){
-        assert.equal(counterValueEl.textContent, '1');
-      });
-      done();
-    });
+   
   });
 };
